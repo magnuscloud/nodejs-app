4a44fa668917da5018d23bff1338e702f06f580b
magnuscloud <arivazhagan.vp@gmail.com>
2020-04-19 17:04:04 +0530

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ console.log("Magnus server starting....");
 var handler = function(request, response) {
     console.log("Received request from " + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("Congratualations! Your sample NodeJS app is working!!! This is v1 of app." + "\n" + "The server you've reached now: " + os.hostname() + "\n");
+    response.end("Congratualations! Your sample NodeJS app is working!!!" + "\n" + "This is v1 of app. The server you've reached now: " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(80);
